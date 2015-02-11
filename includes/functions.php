6bac3993eeedeff781ac4a64f2b93b12b292b956
abzu1180 <mabzun@csumb.edu>
2015-02-11 19:30:54 +0000

    Generate Hand

--- includes/functions.php
+++ includes/functions.php
@@ -3,4 +3,30 @@
 /*
  * To change this template use Tools | Templates.
  */
+
+function generateHand(){
+    
+    $arraySuits = array();
+    $arraySuits = array("clubs","diamonds", "hearts", "spades");
+    
+    $arrayHand = array();
+    
+    while(count($arrayHand) < 4){
+        $card = rand(1,13);
+
+        if(!in_array($card, $arrayHand)){
+            $arrayHand[] = $card;
+        }
+    
+    }
+    // display arrayhand
+    foreach($arrayHand as $card){
+        $suit = array_rand($arraySuits);
+       echo "<img src='img/cards/".generateRandomSuit()."/$num1.png'/>";
+    }
+    
+}
+function generateTotal(){
+    
+}
 ?>
