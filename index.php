5ee29c366d02dd57acd6396d34a7a9743a51c0a6
saymtfmtfmtf <mfenton@csumb.edu>
2015-02-11 21:58:41 +0000

    Added the basic methods to display and add information into an array

--- index.php
+++ index.php
@@ -1,20 +1,33 @@
 <?php
 	include ('includes/functions.php');
-    
+    static $totalHand = array();
+
     function displayFaces() {
         $faces = array();
         for($i = 0; $i < 4; $i++) {
-            array_push($faces, "<img src='player_faces/$i.png'/>");
+            array_push($faces, "<img src='img/player_faces/$i.png'/>");
         }
         
         return $faces;
     }
+    
+    function addToHand($hand) {
+        global $totalHand;
+        $totalHand[] = $hand;
+        
+    }
+    
+    function getHand() {
+        global $totalHand;
+        return $totalHand;
+    }
+
 ?>
 <DOCTYPE! HTML>
 <html>
 <head>
 <title>Lab 2 Team</title>
-<link href="CSS/style.css" rel="stylesheet">
+<link href="CSS/styles.css" rel="stylesheet">
 </head>
 <body>
 <div id="wrapper">
@@ -22,13 +35,15 @@
 
 	<?php
         $faceArr = displayFaces();
-        
-        for($i = 0; $i < 4; $i++) { $hand = generateHand(); }
+       
+        for($i = 0; $i < 4; $i++) {
+            echo $faceArr[$i]; // display face
+            $hand = generateHand(); // generate the players hand
+            $sum = generateTotal($hand); // get the total of the hand
+            addToHand($sum);
+            echo $sum . "<br/>";
+        }
 		
-
-
-
-		$total = generateTotal($hand);
 
 	?>
 
